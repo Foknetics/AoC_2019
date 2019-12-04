01624331d2071026696643b3903d273e93ccfa0a
Connor Bratten <connorabratten@gmail.com>
2019-12-04 10:32:14 -0800

    Prep intcode to be able to handle basic math ops easily

--- day_02/intcode.py
+++ day_02/intcode.py
@@ -1,15 +1,16 @@
+math_ops = {
+    1: '+',
+    2: '*'
+}
+
+
 def process(memory):
     instruction_pointer = 0
     while True:
         opcode = memory[instruction_pointer]
-        if opcode == 1 or opcode == 2:
-            parm1 = memory[instruction_pointer+1]
-            parm2 = memory[instruction_pointer+2]
-            parm3 = memory[instruction_pointer+3]
-            if opcode == 1:
-                memory[parm3] = memory[parm1] + memory[parm2]
-            else:
-                memory[parm3] = memory[parm1] * memory[parm2]
+        if opcode in math_ops.keys():
+            parm1, parm2, parm3 = [memory[instruction_pointer + offset] for offset in range(1, 4)]
+            memory[parm3] = eval(f'{memory[parm1]} {math_ops[opcode]} {memory[parm2]}')
             instruction_pointer += 4
         elif opcode == 99:
             return memory[0]
