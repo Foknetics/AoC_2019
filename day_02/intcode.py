64d22565ba433757f0c6d15a9a95590e48a09a7f
Connor Bratten <connorabratten@gmail.com>
2019-12-04 18:12:13 -0800

    Swap to underlying operators vs eval to improve intcode speed

--- day_02/intcode.py
+++ day_02/intcode.py
@@ -1,6 +1,12 @@
+from operator import (
+    add,
+    multiply
+)
+
+
 math_ops = {
-    1: '+',
-    2: '*'
+    1: add,
+    2: multiply
 }
 
 
@@ -10,7 +16,7 @@ def process(memory):
         opcode = memory[instruction_pointer]
         if opcode in math_ops.keys():
             parm1, parm2, parm3 = [memory[instruction_pointer + offset] for offset in range(1, 4)]
-            memory[parm3] = eval(f'{memory[parm1]} {math_ops[opcode]} {memory[parm2]}')
+            memory[parm3] = math_ops[opcode](memory[parm1], memory[parm2])
             instruction_pointer += 4
         elif opcode == 99:
             return memory[0]
