8ff472dc980a1a0fdff5889a2e52ae56ca634ce2
Connor Bratten <connorabratten@gmail.com>
2019-12-04 22:31:21 -0800

    Refactor down parm retrieval including modes

--- day_05/intcode.py
+++ day_05/intcode.py
@@ -4,6 +4,12 @@ from operator import (
 )
 
 
+math_ops = {
+    1: add,
+    2: mul
+}
+
+
 def jump_if_true(pointer, parm1, parm2):
     if parm1 != 0:
         return parm2
@@ -18,6 +24,12 @@ def jump_if_false(pointer, parm1, parm2):
         return pointer+3
 
 
+jump_ops = {
+    5: jump_if_true,
+    6: jump_if_false
+}
+
+
 def less_than(parm1, parm2):
     if parm1 < parm2:
         return 1
@@ -29,16 +41,6 @@ def equal(parm1, parm2):
         return 1
     return 0
 
-
-math_ops = {
-    1: add,
-    2: mul
-}
-
-jump_ops = {
-    5: jump_if_true,
-    6: jump_if_false
-}
 
 equality_ops = {
     7: less_than,
@@ -55,23 +57,28 @@ def parse_opcode(opcode):
     return instruction, modes
 
 
+def n_parms(n, memory, pointer, modes):
+    parms = []
+    for offset in range(1, n+1):
+        if len(parms) == 2:
+            parms.append(memory[pointer+offset])
+            continue
+        if modes[offset-1] == 0:
+            parms.append(memory[memory[pointer+offset]])
+        else:
+            parms.append(memory[pointer+offset])
+    if len(parms) == 1:
+        return parms[0]
+    return parms
+
+
 def process(memory):
     instruction_pointer = 0
     while True:
         opcode = memory[instruction_pointer]
         instruction, modes = parse_opcode(opcode)
         if instruction in math_ops.keys():
-            if modes[0] == 0:
-                parm1 = memory[memory[instruction_pointer+1]]
-            else:
-                parm1 = memory[instruction_pointer+1]
-            if modes[1] == 0:
-                parm2 = memory[memory[instruction_pointer+2]]
-            else:
-                parm2 = memory[instruction_pointer+2]
-
-            parm3 = memory[instruction_pointer+3]
-
+            parm1, parm2, parm3 = n_parms(3, memory, instruction_pointer, modes)
             memory[parm3] = math_ops[instruction](parm1, parm2)
             instruction_pointer += 4
         elif instruction == 3:
@@ -79,33 +86,14 @@ def process(memory):
             memory[parm1] = int(input('Input requested: '))
             instruction_pointer += 2
         elif instruction == 4:
-            if modes[0] == 0:
-                parm1 = memory[memory[instruction_pointer+1]]
-            else:
-                parm1 = memory[instruction_pointer+1]
+            parm1 = n_parms(1, memory, instruction_pointer, modes)
             print(f'Output: {parm1}')
             instruction_pointer += 2
         elif instruction in jump_ops.keys():
-            if modes[0] == 0:
-                parm1 = memory[memory[instruction_pointer+1]]
-            else:
-                parm1 = memory[instruction_pointer+1]
-            if modes[1] == 0:
-                parm2 = memory[memory[instruction_pointer+2]]
-            else:
-                parm2 = memory[instruction_pointer+2]
+            parm1, parm2 = n_parms(2, memory, instruction_pointer, modes)
             instruction_pointer = jump_ops[instruction](instruction_pointer, parm1, parm2)
         elif instruction in equality_ops.keys():
-            if modes[0] == 0:
-                parm1 = memory[memory[instruction_pointer+1]]
-            else:
-                parm1 = memory[instruction_pointer+1]
-            if modes[1] == 0:
-                parm2 = memory[memory[instruction_pointer+2]]
-            else:
-                parm2 = memory[instruction_pointer+2]
-
-            parm3 = memory[instruction_pointer+3]
+            parm1, parm2, parm3 = n_parms(3, memory, instruction_pointer, modes)
             memory[parm3] = equality_ops[instruction](parm1, parm2)
             instruction_pointer += 4
         elif instruction == 99:
